0e58a9d5716c8b16799e608cf2b232695b594d8a
Azat Akhmetyanov <a.r.akhmetyanov@gmail.com>
2020-11-09 22:56:55 +0300

    Load TuringMachine in main class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,16 @@
+import formallang.TuringMachine;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Application {
     public static void main(String[] args) {
-
+        Path turingMachinePath = Paths.get("src", "main", "resources", "turing_machine.txt");
+        try {
+            TuringMachine tm = TuringMachineUtils.loadTuringMachine(turingMachinePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
